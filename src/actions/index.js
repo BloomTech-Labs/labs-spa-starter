719b29f84e7609b7b413350a1677823b844954e7
Ryan Hamblin <8798259+ryan-hamblin@users.noreply.github.com>
2020-06-01 10:04:10 -0600

    considers some patterns on redux

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+// import all of your actions into this file, and export them back out. 
+// This allows for the simplification of flow when importing actions into your components throughout your app.
+// Actions should be focused to a single purpose. 
+// You can have multiple action creators per file if it makes sense to the purpose those action creators are serving. 
+// Declare action TYPES at the top of the file
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+// import all of your reducers into this file, and export them back out. 
+// This allows for the simplification of flow when importing reducers into your actions throughout your app.
+
+
