94d31fe0893604519b61e9a94abb7da73db3dadf
Ryan Hamblin <8798259+ryan-hamblin@users.noreply.github.com>
2020-06-24 07:14:03 -0600

    adds comments

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ ReactDOM.render(
 );
 
 function App() {
+  // The reason to declare App this way is so that we can use any helper functions we'd need for business logic, in our case auth.
   // React Router has a nifty useHistory hook we can use at this level to ensure we have security around our routes.
   const history = useHistory();
 
