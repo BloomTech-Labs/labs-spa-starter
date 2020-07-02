cddf622019529480daa84d519bbd8f4508aa8155
Jake Mallory <jake@tinomen.com>
2020-07-01 22:37:13 -0600

    add secure api

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,39 @@
 import axios from "axios";
-// we will define a bunch of API calls here.
 
-const sleep = (time) =>
-  new Promise((resolve) => {
+// we will define a bunch of API calls here.
+const apiUrl = `${process.env.REACT_APP_API_URI}/profiles`;
+
+const sleep = time =>
+  new Promise(resolve => {
     setTimeout(resolve, time);
   });
 
 const getExampleData = () => {
   return axios
     .get(`https://jsonplaceholder.typicode.com/photos?albumId=1`)
-    .then((response) => response.data);
+    .then(response => response.data);
 };
 
-export { sleep, getExampleData };
+const getAuthHeader = authState => {
+  if (!authState.isAuthenticated) {
+    throw new Error("Not authenticated");
+  }
+  return { Authorization: `Bearer ${authState.accessToken}` };
+};
+
+const apiAuthGet = authHeader => {
+  return axios.get(apiUrl, { headers: authHeader });
+};
+
+const getProfileData = authState => {
+  try {
+    return apiAuthGet(getAuthHeader(authState)).then(response => response.data);
+  } catch (error) {
+    return new Promise(() => {
+      console.log(error);
+      return [];
+    });
+  }
+};
+
+export { sleep, getExampleData, getProfileData };
