eefead0218bd1772dd5435b12c862127210b860c
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-07-23 21:59:02 +0100

    Adds Ant Design Loading Spinner To Storybook

--- src/stories/LoadingComponent.stories.js
+++ src/stories/LoadingComponent.stories.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { LoadingComponent } from '../components/common';
+import { Spin } from 'antd';
+
+import 'antd/dist/antd.css';
 
 export default { title: 'Loading Component' };
 
 export const loadingComponent = () => <LoadingComponent message="...loading" />;
+
+export const antLoadingComponent = () => <Spin tip="Loading..." />;
