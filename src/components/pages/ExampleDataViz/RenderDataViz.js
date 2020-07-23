e33e9de107d84de895f36888a1cf0bf137f543e1
Ryan Hamblin <8798259+ryan-hamblin@users.noreply.github.com>
2020-07-23 16:31:48 -0600

    updates render useEffect for DataViz adds dirs for state

--- src/components/pages/ExampleDataViz/RenderDataViz.js
+++ src/components/pages/ExampleDataViz/RenderDataViz.js
@@ -3,37 +3,32 @@ import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js';
 import { getDSData } from '../../../api';
 
+const initialState = {
+  data: [],
+  layout: {},
+  frames: [],
+  config: {
+    displaylogo: false,
+    displayModeBar: false,
+  },
+};
+
 function DataViz(props) {
-  const initialState = {
-    data: [],
-    layout: {},
-    frames: [],
-    config: {
-      displaylogo: false,
-      displayModeBar: false,
-    },
-  };
   const [data, setData] = useState(initialState);
   const [figure, setFigure] = useState(null);
 
   useEffect(() => {
-    let loadingData = true;
     function fetchDSData() {
       getDSData(props.url)
         .then(res => {
-          if (loadingData) {
-            setData(res);
-          }
+          setData(res);
         })
         .catch(err => {
-          loadingData = false;
           return setData(initialState);
         });
     }
     fetchDSData();
-    return () => (loadingData = false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [props.url]);
 
   return (
     <Plot
