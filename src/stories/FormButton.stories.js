08c7474176bfb946f0c3118351534a18ca775b6f
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-07-23 20:58:13 +0100

    Adds Ant Design Button To Storybook

--- src/stories/FormButton.stories.js
+++ src/stories/FormButton.stories.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Button } from 'antd';
 import { FormButton } from '../components/common';
+
+import 'antd/dist/antd.css';
 
 export default { title: 'Form Button' };
 
 export const basicUsage = () => <FormButton buttonText="hello" />;
+
+export const usageWithAnt = () => <Button>Submit</Button>;
