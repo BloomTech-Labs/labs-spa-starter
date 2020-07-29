fd6caae8a1e4ff0e122ae714983c0424abee1d18
Ryan Hamblin <8798259+ryan-hamblin@users.noreply.github.com>
2020-07-29 10:43:18 -0600

    adds notes implementation for storybook

--- src/stories/FormButton.stories.js
+++ src/stories/FormButton.stories.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button } from 'antd';
 import { FormButton } from '../components/common';
+import markdown from './story_descriptions/buttonNotes.md';
 
 import 'antd/dist/antd.css';
 
-export default { title: 'Form Button' };
+export default { title: 'Form Button', parameters: { notes: markdown } };
 
 export const basicUsage = () => <FormButton buttonText="hello" />;
 
---
+++ src/stories/story_descriptions/buttonNotes.md
@@ -0,0 +1,8 @@
+# Button
+
+- To use the common `<Button></Button>` component from `antd` you'll need to import blah
+
+```js
+import foo from 'bar';
+function javaScript() {}
+```
