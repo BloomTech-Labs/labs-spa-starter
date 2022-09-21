2d1bbf41db2a97574c62cc3d6745cc0b2e644ead
Ryan Hamblin <8798259+ryan-hamblin@users.noreply.github.com>
2022-09-21 13:48:47 -0600

    adds dummyData for testing

---
+++ src/dummyData/imagesTestData.js
@@ -0,0 +1,34 @@
+const imageData = {
+  results: [
+    {
+      id: 'qNhstTawQrI',
+      description:
+        'A lovely lady angler took our photographer down to the river in Eleven Mile Canyon, Lake George, CO to do a little fly fishing. This woman is a true badass! Within the first 10 mins of wading out in the water, @pixiek8 (as she is known on Instagram) caught this beauty of a trout. \r\n\r\nPS That’s one of our durable/waterproof custom stickers on her fly box.',
+      urls: {
+        thumb:
+          'https://images.unsplash.com/photo-1485452499676-62ab02c20e83?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNTE1MDl8MHwxfHNlYXJjaHwxfHxyYWluYm93JTIwdHJvdXR8ZW58MHx8fHwxNjYzNzcxOTE1&ixlib=rb-1.2.1&q=80&w=200',
+      },
+      alt_description: 'person holding gray and beige fish',
+    },
+    {
+      id: 'azWmN1vjxzM',
+      description: null,
+      urls: {
+        thumb:
+          'https://images.unsplash.com/photo-1576468959551-414423712a5c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNTE1MDl8MHwxfHNlYXJjaHwyfHxyYWluYm93JTIwdHJvdXR8ZW58MHx8fHwxNjYzNzcxOTE1&ixlib=rb-1.2.1&q=80&w=200',
+      },
+      alt_description: 'group of green fish',
+    },
+    {
+      id: 'F26bq3Zv9p0',
+      description: null,
+      urls: {
+        thumb:
+          'https://images.unsplash.com/photo-1573956869478-7f1f3bed8d14?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNTE1MDl8MHwxfHNlYXJjaHwzfHxyYWluYm93JTIwdHJvdXR8ZW58MHx8fHwxNjYzNzcxOTE1&ixlib=rb-1.2.1&q=80&w=200',
+      },
+      alt_description: 'man holding brown fish',
+    },
+  ],
+};
+
+export default imageData;
